76cb7344441051f56c00d4a4eb1618724d4281cb
cata36 <cataroitman@gmail.com>
2026-05-26 15:57:43 -0300

    café de especialidad final version

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,18 @@ let totalTexto: string = preguntar('Total de la cuenta: ');
 let esEstudiante: string = preguntar('¿Son estudiantes de ORT? si/no: ');
 let personasTexto: string = preguntar('Dividir la cuenta entre: ');
 
-let totalPorPersona: number = 0; // COMPLETAR
+let totalPorPersona: number = 0; 
+
+if (esEstudiante === 'si') 
+{
+    totalPorPersona = (Number(totalTexto) * 0.9) / Number(personasTexto);
+}
+else if (esEstudiante === "no"){
+    totalPorPersona = Number(totalTexto)  / Number(personasTexto);
+
+}
+else {
+    console.log("Ingrese un valor valido")
+}
+
 console.log(`Total por persona: $${totalPorPersona}`);
